a935edbeb030887293bd9b5cf7f48e09fc475820
SaminYeaser <saminyeaser1@gmail.com>
2022-05-14 02:44:01 +0600

    creating dashboard

--- lib/Screens/home/home_page.dart
+++ lib/Screens/home/home_page.dart
@@ -1,10 +1,33 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fearless/Screens/Welcome/components/body.dart';
+import 'package:fearless/constants.dart';
 import 'package:fearless/model/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
+
+Widget? menuItem(String name, IconData icon){
+  return InkWell(
+    onTap: (){},
+    child: Container(
+      height: 40,
+      color: Colors.grey[300],
+      child: Row(
+        children:  [
+          Expanded(child: Icon(icon, color: kPrimaryColor,size: 16,)),
+          const SizedBox(width: 30),
+          Expanded(
+            child: Text(name, style: const TextStyle(
+                fontSize: 16
+            ),),
+          )
+        ],
+      ),
+    ),
+  );
+}
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -16,6 +39,7 @@ class _HomePageState extends State<HomePage> {
 
   User? user = FirebaseAuth.instance.currentUser;
   UserModel loggedInUserModel = UserModel();
+  bool selected = false;
 
   @override
   void initState() {
@@ -34,9 +58,27 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        backgroundColor: kPrimaryLightColor,
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Column(
+                children: [
+                  SizedBox(height: 200,),
+                  menuItem('home', Icons.home)!
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
       appBar: AppBar(
-        automaticallyImplyLeading: false,
+        backgroundColor: kPrimaryColor,
+        // automaticallyImplyLeading: false,
         title: const Text('Fearless'),
+        titleSpacing: 0,
         actions: [
           IconButton(onPressed: () async{
             FirebaseAuth.instance.signOut();
